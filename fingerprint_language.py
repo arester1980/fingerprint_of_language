0264b4c224c5f976c63b538b9bccd0871f66c5f4
Arester <temultik@gmail.com>
2020-04-10 16:19:57 +0300

    convert to procents

--- fingerprint_language.py
+++ fingerprint_language.py
@@ -2,7 +2,7 @@
 считаем каких букв в строке/тексте больше
 деалем графику"""
 
-from collections import Counter 
+from collections import Counter
 
 f = open('fingerprint_language.txt', 'r')
 s = f.read()
@@ -13,9 +13,10 @@ d = dict(Counter(s1))
 list_keys = list(d.keys())
 list_keys.sort()
 
-for i in list_keys:
- 	print(i, ":", d[i])
+procent = d[max(d, key=d.get)]
 
- 	
+for i in list_keys:
+    d[i] = round((d[i] / 100 * procent), 2)
+    print(f'{i}, ":", {d[i]}%')
 
 f.close()
